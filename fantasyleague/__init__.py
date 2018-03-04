c9f5ddddfc61247a83710ce3b0ad58787731b07a
kodycode <kodythach@gmail.com>
2018-03-04 13:50:45 -0500

    Updated version number

--- fantasyleague/__init__.py
+++ fantasyleague/__init__.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'fantasyleague'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'Kody Thach'
 __repo__ = 'https://github.com/kodycode/League-of-Legends-Fantasy-API-Wrapper'
 __license__ = 'MIT License'
 
 from .core import FantasyLeague
+from .season import SeasonData
