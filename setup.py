1895d3418e79792b5542c55247f03850707302fc
kodycode <kodythach@gmail.com>
2018-03-05 12:18:37 -0500

    Finished PlayerData class

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 from setuptools import setup
 
 setup(
-    name='fantasyleague',
-    version='1.1.0',
+    name='fantasylcs',
+    version='1.1.1',
     url='https://github.com/kodycode/Fantasy-LCS-API-Wrapper',
     author='Kody Thach',
     license='MIT License',
-    packages=['fantasyleague'],
+    packages=['fantasylcs'],
     description='An unofficial FantasyLCS API Wrapper.',
     long_description=open('README.md', 'r').read(),
     install_requires=['requests==2.18.4'],
