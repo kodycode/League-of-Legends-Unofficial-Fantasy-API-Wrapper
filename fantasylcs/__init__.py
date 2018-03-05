2a265dddd5660c3071a1c687bde009c60990e73d
kodycode <kodythach@gmail.com>
2018-03-05 12:24:02 -0500

    Changed __init__ title

--- fantasylcs/__init__.py
+++ fantasylcs/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__title__ = 'fantasyleague'
+__title__ = 'fantasylcs'
 __version__ = '1.1.1'
 __author__ = 'Kody Thach'
 __repo__ = 'https://github.com/kodycode/League-of-Legends-Fantasy-API-Wrapper'
