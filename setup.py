8c150019a74cc24fb5406e6ad35dff02529cd7e4
kodycode <kodythach@gmail.com>
2018-07-19 21:42:20 -0400

    v1.2
    * Markdown enabled
    * Updated README.md

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup
 
 setup(
     name='fantasylcs',
-    version='1.1.2',
+    version='1.2',
     url='https://github.com/kodycode/Fantasy-LCS-API-Wrapper',
     author='Kody Thach',
     license='MIT License',
     packages=['fantasylcs'],
     description='An unofficial FantasyLCS API Wrapper.',
     long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     install_requires=['requests==2.18.4'],
 )
