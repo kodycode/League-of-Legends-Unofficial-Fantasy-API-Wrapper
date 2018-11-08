bdfb4f7c54245cdeeba58a9bd22724a622047f01
Kodycode <kodythach@gmail.com>
2018-11-08 00:39:57 -0500

    v1.2.2
    Updated requests (>=2.20.0)

--- fantasylcs/__init__.py
+++ fantasylcs/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'fantasylcs'
-__version__ = '1.1.2'
+__version__ = '1.2.2'
 __author__ = 'Kody Thach'
 __repo__ = 'https://github.com/kodycode/League-of-Legends-Fantasy-API-Wrapper'
 __license__ = 'MIT License'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='fantasylcs',
-    version='1.2.1',
+    version='1.2.2',
     url='https://github.com/kodycode/Fantasy-LCS-API-Wrapper',
     author='Kody Thach',
     license='MIT License',
@@ -13,5 +13,5 @@ setup(
     description='An unofficial FantasyLCS API Wrapper.',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
-    install_requires=['requests==2.18.4'],
+    install_requires=['requests==2.20.0'],
 )
